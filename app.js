ddd3950e5dbc907fc80153605890c94943bb6379
Ayush Gade <ayushgade100@gmail.com>
2026-02-17 18:36:43 +0530

    updated index.html and app.js files

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from DevOps Intern\n');
+    fs.readFile('./index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("Server Error");
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
 });
 
 server.listen(3000, () => {
-  console.log('Server running on port 3000');
+    console.log("Server running on port 3000");
 });
